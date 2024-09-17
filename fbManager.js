e8a62a9ecab0e52a821a420de999f46144278b15
jcmaker0627 <jcmaker0627@gmail.com>
2024-09-17 16:34:05 +0900

    fb typeof window

--- fbManager.js
+++ fbManager.js
@@ -13,6 +13,17 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
+if (!firebase.apps.length) {
+  // Firebase 앱 초기화
+  firebase.initializeApp(firebaseConfig);
+
+  // Firebase 애널리틱스 초기화 (필요한 경우에만 사용)
+}
+
+if (typeof window !== "undefined") {
+  firebase.analytics();
+}
+
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
